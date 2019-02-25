abc2f16b7f72842d1c14ab2c80cc9b7de4be319e
Manish Tanwar <manish.tanwar19@gmail.com>
2019-02-25 18:08:02 +0530

    started the report

---
+++ A1/Report/codes/1.c
@@ -0,0 +1,13 @@
+#include "types.h"
+#include "defs.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "x86.h"
+#include "proc.h"
+#include "spinlock.h"
+
+struct {
+  struct spinlock lock;
+  struct proc proc[NPROC];
+} ptable;
